e02fe0da43b3c92c6c3f44433279369831fcbddb
JoostdK <joost.dekat@hotmail.com>
2025-10-12 18:03:35 +0200

    added readme

--- run.sh
+++ run.sh
@@ -7,4 +7,4 @@ set -euo pipefail
 # Get the directory this script is in
 SCRIPT_DIR="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 # Install all required packages from list
-"$SCRIPT_DIR/scripts/install-packages.sh"
+"$SCRIPT_DIR/scripts/install_packages.sh"
