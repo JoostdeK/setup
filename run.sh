f777d59cf72db995e4d0c5985d6016fdf7494520
JoostdK <joost.dekat@hotmail.com>
2025-10-12 20:42:05 +0200

    added readme

--- run.sh
+++ run.sh
@@ -7,4 +7,7 @@ set -euo pipefail
 # Get the directory this script is in
 SCRIPT_DIR="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 # Install all required packages from list
-"$SCRIPT_DIR/scripts/install_packages.sh"
+"$SCRIPT_DIR/scripts/install-packages.sh"
+"$SCRIPT_DIR/scripts/install-config.sh"
+
+##   install catpucchin theme in kitty.
