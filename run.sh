624786319357cb6725739ad0462072489cfc8c21
JoostdK <joost.dekat@hotmail.com>
2025-10-12 22:11:40 +0200

    added readme

--- run.sh
+++ run.sh
@@ -9,7 +9,7 @@ SCRIPT_DIR="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 # Install all required packages from list
 "$SCRIPT_DIR/scripts/install-packages.sh"
 "$SCRIPT_DIR/scripts/install-config.sh"
-"$SCRIPT_DIR/scripts/configure-startup.sh" jvdb
+sudo "$SCRIPT_DIR/scripts/configure-startup.sh" jvdb
 "$SCRIPT_DIR/scripts/finish.sh"
 
 ##   install catpucchin theme in kitty.
