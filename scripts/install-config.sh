bc640132d1daddb96540916e6b952932f87f2720
JoostdK <joost.dekat@hotmail.com>
2025-10-12 21:04:00 +0200

    added readme

--- scripts/install-config.sh
+++ scripts/install-config.sh
@@ -2,8 +2,9 @@
 
 ## clone config files
 git init --bare $HOME/.jb
-alias jb='/usr/bin/git --git-dir=$HOME./jb/ --work-tree=$HOME'
 
 echo "alias jb='/usr/bin/git --git-dir=\$HOME/.jb/ --work-tree=\$HOME'" >>"$HOME/.zshrc"
-
+function jb {
+  /usr/bin/git --git-dir=$HOME/.jb/ --work-tree=$HOME $@
+}
 jb config --local status.showUntrackedFiles no
