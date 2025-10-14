69a57a593d02f58de7dd61e0934698587af0fc6f
JoostdK <joost.dekat@hotmail.com>
2025-10-14 20:42:47 +0200

    cp bg

--- scripts/cp-files.sh
+++ scripts/cp-files.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/bash
 
 PARENT_DIR="$(realpath "$(dirname "$0")/..")"
-sudo mkdir -p /usr/share/hypr
-sudo cp "$(PARENT_DIR)/images/bg.png" /usr/share/hypr/bg.png
+sudo mkdir -p $HOME/.config/system/background
+sudo cp "$(PARENT_DIR)/images/bg.png" /$HOME/.config/system/background/bg.png
