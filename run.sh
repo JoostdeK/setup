0ad55b5ac349ef79924eed6598fa15886a491411
JoostdeK <110180328+JoostdeK@users.noreply.github.com>
2025-10-15 20:03:48 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -7,7 +7,7 @@ set -euo pipefail
 # Get the directory this script is in
 SCRIPT_DIR="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 # Install all required packages from list
-#"$SCRIPT_DIR/scripts/install-packages.sh"
+"$SCRIPT_DIR/scripts/install-packages.sh"
 "$SCRIPT_DIR/scripts/install-config.sh"
 sudo "$SCRIPT_DIR/scripts/configure-startup.sh" jvdb
 "$SCRIPT_DIR/scripts/finish.sh"
